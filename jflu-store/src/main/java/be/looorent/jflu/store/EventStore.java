b47c7ed3e2a4df76c4a370f12f5effded0087f92
Lorent Lempereur <lorent.lempereur.dev@gmail.com>
2016-11-28 22:38:15 +0100

    Liquibase configuration

--- jflu-store/src/main/java/be/looorent/jflu/store/EventStore.java
+++ jflu-store/src/main/java/be/looorent/jflu/store/EventStore.java
@@ -5,12 +5,16 @@ import be.looorent.jflu.subscriber.RabbitMQListener;
 import be.looorent.jflu.subscriber.RabbitMQSubscriptionRepository;
 import be.looorent.jflu.subscriber.SubscriptionScanner;
 import liquibase.Liquibase;
+import liquibase.changelog.DatabaseChangeLog;
 import liquibase.database.Database;
 import liquibase.database.DatabaseFactory;
 import liquibase.database.jvm.JdbcConnection;
 import liquibase.exception.DatabaseException;
 import liquibase.exception.LiquibaseException;
+import liquibase.resource.ClassLoaderResourceAccessor;
 import liquibase.resource.FileSystemResourceAccessor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.sql.*;
 import java.util.Properties;
@@ -24,25 +28,31 @@ import static be.looorent.jflu.subscriber.RabbitMQPropertyName.readPropertiesFro
  */
 public class EventStore {
 
+    private final static Logger LOG = LoggerFactory.getLogger(EventStore.class);
+
     public static void main(String... args) throws SQLException, LiquibaseException {
         migrateDatabase();
         listenToQueue();
     }
 
     private static void migrateDatabase() throws SQLException, LiquibaseException {
+        LOG.info("Migrating database...");
         try (Connection connection = createDatabaseConnection()) {
             Database database = DatabaseFactory.getInstance().findCorrectDatabaseImplementation(
                     new JdbcConnection(connection)
             );
-            Liquibase liquibase = new Liquibase("/db/changelog.xml", new FileSystemResourceAccessor(), database);
+            Liquibase liquibase = new Liquibase("/db/changelog.xml", new ClassLoaderResourceAccessor(), database);
             liquibase.update("");
         }
+        LOG.info("Migrating database: Done.");
     }
 
     private static void listenToQueue() {
+        LOG.info("Starting listener...");
         RabbitMQConfiguration configuration = new RabbitMQConfiguration(readPropertiesFromEnvironment());
         RabbitMQListener listener = new RabbitMQListener(configuration);
         RabbitMQSubscriptionRepository repository = new RabbitMQSubscriptionRepository(configuration);
         listener.start("be.looorent.jflu.store", new SubscriptionScanner(), repository);
+        LOG.info("Starting listener: Done.");
     }
 }
